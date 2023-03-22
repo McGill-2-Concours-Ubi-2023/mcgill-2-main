083c3bb8c692b7fa0a7e7e0474af25e2ef8a74f7
Ogulcan Kaya <ogulcan.kaya@mail.mcgill.ca>
2023-03-22 15:54:21 -0400

    floating eye

--- Assets/Scripts/DungeonGeneration/EnemySpawn.cs
+++ Assets/Scripts/DungeonGeneration/EnemySpawn.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+    GameObject[] enemyGroups;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject.Instantiate(enemyGroups[Random.Range(0, enemyGroups.Length)], transform.position +new Vector3(0,0.5f),Quaternion.identity);
     }
 
     // Update is called once per frame
