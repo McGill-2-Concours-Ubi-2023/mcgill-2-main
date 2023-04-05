33b16f9e50fe738df81764ce4651632ecb3e128a
Emilie Chen <emilie.hongjun.chen@mail.mcgill.ca>
2023-04-05 01:34:57 -0400

    more changes

--- Assets/Scripts/DungeonGeneration/DungeonRoomCollider.cs
+++ Assets/Scripts/DungeonGeneration/DungeonRoomCollider.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class DungeonRoomCollider : MonoBehaviour
@@ -8,6 +9,10 @@ public class DungeonRoomCollider : MonoBehaviour
     // Start is called before the first frame update
     private async void OnTriggerEnter(Collider other)
     {
+        while (GameManager.isLoading)
+        {
+            await Task.Yield();
+        }
         
         DungeonRoom.activeRoom = transform.parent.GetComponentInParent<DungeonRoom>();
         DungeonRoom.activeRoom.UpdateRoomsLayout();
