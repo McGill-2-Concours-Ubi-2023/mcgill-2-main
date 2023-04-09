56574a1d5c3950d68ec49f47fe4a3e184db207e6
mulan21039 <qingcheng20139@gmail.com>
2023-04-08 22:53:55 -0400

    enemies flash when damaged

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -20,12 +20,31 @@ public class Enemy : MonoBehaviour, I_AI_Trigger
     [SerializeField]
     public GameObject scoringSystem;
 
+    [SerializeField]
+    private Material hurtMaterial;
+    [SerializeField]
+    private Material originalMaterial; 
+    [SerializeField]
+    private float duration;
+    [SerializeField]
+    private int numOfFlashes;
+    [SerializeField]
+    private SkinnedMeshRenderer mr;
+    [SerializeField]
+    private MeshRenderer mr2;
     private void Start()
     {
         //TryGetComponent<EnemyAI>(out ai);
         TryGetComponent<NavMeshAgent>(out agent);
         rb = GetComponent<Rigidbody>();
         enemyHealth.OnDeath += OnEnemyDeath;
+        if (mr != null)
+        {
+            originalMaterial = mr.material;
+        }
+        else originalMaterial = mr2.material;
+        enemyHealth.OnHealthChange += Flash;
+        
         if(agent)
         {
             cachedSpeed = agent.speed;
@@ -49,6 +68,44 @@ public class Enemy : MonoBehaviour, I_AI_Trigger
         }
         scoringSystem.Trigger<IScoringSystemTriggers>(nameof(IScoringSystemTriggers.OnEnemyDeath));
     }
+
+    public void Flash(float change, float current) {
+        if (change >= 0) return;
+        StartCoroutine(Flash());
+    }
+
+    IEnumerator Flash() {
+        int i = 0;
+        float timer = 0f;
+        using HLockGuard healthLock = enemyHealth.Lock();
+        while (i < numOfFlashes)
+        {
+            if (timer < duration)
+            {
+                if (mr != null)
+                {
+                    mr.material = hurtMaterial;
+                }
+                else mr2.material = hurtMaterial;
+            }
+            else if (timer < duration * 2)
+            {
+                if (mr != null)
+                {
+                    mr.material = originalMaterial;
+                }
+                else mr2.material = originalMaterial;
+            }
+            else
+            {
+                timer = 0f;
+                i++;
+            }
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
 
     public void DisableAgent()
     {
