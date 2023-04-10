f316804997b577bb6fd4ad0903d85c4cfb46f191
O. Kaya <ogulcankaya117@gmail.com>
2023-04-10 04:01:55 -0400

    deterministic spawns made, it destroyed everything

--- Assets/Scripts/DungeonGeneration/EnemySpawn1.cs
+++ Assets/Scripts/DungeonGeneration/EnemySpawn1.cs
@@ -9,18 +9,35 @@ public class EnemySpawn1 : MonoBehaviour
     public Vector3[] spawnpoints;
     public VisualEffect volumeFog;
     private GameObject player;
+    public bool debugSpawn=false;
+    public bool doubleSpawn = true;
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Vector3 sp in spawnpoints)
+        if (debugSpawn)
         {
-            if (Random.Range(0, 3) == 0)
+            foreach (Vector3 sp in spawnpoints)
             {
-                int rand = Random.Range(1, 2);
-                for (int i = 0; i <= rand; i++)
+                if (Random.Range(0, 3) == 0)
                 {
-                    GameObject.Instantiate(enemies[Random.Range(0, enemies.Length)], transform.position + sp + new Vector3(0, 0.5f), Quaternion.identity);
+                    int rand = Random.Range(1, 2);
+                    for (int i = 0; i <= rand; i++)
+                    {
+                        GameObject.Instantiate(enemies[Random.Range(0, enemies.Length)], transform.position + sp + new Vector3(0, 0.5f), Quaternion.identity);
+                    }
                 }
+            }
+        }
+        else
+        {
+            foreach (Vector3 sp in spawnpoints)
+            {
+
+                int rand = Random.Range(0, 5);
+                GameObject.Instantiate(enemies[Random.Range(0, enemies.Length)], transform.position + sp + new Vector3(0, 0.5f), Quaternion.identity);
+                if (rand == 4 && doubleSpawn)
+                    GameObject.Instantiate(enemies[Random.Range(0, enemies.Length)], transform.position + sp + new Vector3(0, 0.5f), Quaternion.identity);
+
             }
         }
     }
