b39bbd1cdd59fdc69d76fbcf9a36ea103b259969
Alexandre Chiasera <alexandre.chiasera@gmail.com>
2023-04-10 15:22:42 -0400

    MusicFix and Boss health

--- Assets/Scripts/Enemy/FinalBoss/FinalBossController.cs
+++ Assets/Scripts/Enemy/FinalBoss/FinalBossController.cs
@@ -67,7 +67,7 @@ public class FinalBossController : MonoBehaviour, IBossTriggers, IHealthObserver
     {
         // send to UI
         Debug.Log($"Boss health change : {change} to : {currentHealth}");
-        fillBar.fillAmount = currentHealth;
+        fillBar.fillAmount = Mathf.Clamp01(currentHealth / 300);
     }
 
     public async void OnDeath()
@@ -278,7 +278,7 @@ public class FinalBossController : MonoBehaviour, IBossTriggers, IHealthObserver
         yield return new WaitForSeconds(lazerbeamDuration);
         lazer1.SendEvent("OnLazerStop");
         lazer2.SendEvent("OnLazerStop");
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.0f);
         Destroy(lazer1.gameObject);
         Destroy(lazer2.gameObject);
     }
@@ -299,7 +299,7 @@ public class FinalBossController : MonoBehaviour, IBossTriggers, IHealthObserver
         vibration.SoftVibration();
         yield return new WaitForSeconds(lazerbeamDuration);
         lazer1.SendEvent("OnLazerStop");
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.0f);
         Destroy(lazer1.gameObject);
     }
     IEnumerator AwakeLazer(GameObject lazer, float lazerChargeTime, float lazerbeamDuration)
@@ -315,7 +315,7 @@ public class FinalBossController : MonoBehaviour, IBossTriggers, IHealthObserver
         vibration.SoftVibration();
         yield return new WaitForSeconds(lazerbeamDuration);
         lazer1.SendEvent("OnLazerStop");
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.0f);
         Destroy(lazer1.gameObject);
     }
 
@@ -376,7 +376,7 @@ public class FinalBossController : MonoBehaviour, IBossTriggers, IHealthObserver
         }
         lazer1.SendEvent("OnLazerStop");
         lazer2.SendEvent("OnLazerStop");
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.0f);
         Destroy(lazer1.gameObject);
         Destroy(lazer2.gameObject);
     }
