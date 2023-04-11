751c386bce047afca0538e49685d40645a8ac528
Alexandre Chiasera <alexandre.chiasera@gmail.com>
2023-04-11 06:52:08 -0400

    ..

--- Assets/Scripts/Enemy/FinalBoss/LazerPlayerCollider.cs
+++ Assets/Scripts/Enemy/FinalBoss/LazerPlayerCollider.cs
@@ -8,6 +8,7 @@ public class LazerPlayerCollider : MonoBehaviour
     private CinemachineCameraShake cameraShake;
     private Vibration vibration;
     private LazerBeamCollider parentCollider;
+    private MainCharacterController player;
     // Start is called before the first frame update
 
     private void OnEnable()
@@ -15,17 +16,21 @@ public class LazerPlayerCollider : MonoBehaviour
         parentCollider = GetComponentInParent<LazerBeamCollider>();
         cameraShake = parentCollider.cameraShake;
         vibration = parentCollider.vibration;
+        player = FindObjectOfType<MainCharacterController>();
     }
     private async void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.GetComponent<Health>().TakeDamage(1);
-            if (!other.GetComponent<Health>().IsInvincible())
-            vibration.SharpVibration();
-            cameraShake.StandardCameraShake(4.0f, 1.0f, 2.0f, 0);
-            await Task.Delay(500);
-            cameraShake.StopCameraShake();
+            Health playerHealth = player.GetComponent<Health>();
+            if (!playerHealth.IsInvincible())
+            {
+                playerHealth.TakeDamage(1);
+                vibration.SharpVibration();
+                cameraShake.StandardCameraShake(4.0f, 1.0f, 2.0f, 0);
+                await Task.Delay(500);
+                cameraShake.StopCameraShake();
+            }           
         }
     }
 }
